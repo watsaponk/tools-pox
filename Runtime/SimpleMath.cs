ca86d332ef3e15a3f71956b934888d40dd9c9740
Watsapon Kampong <k.watsapon@gmail.com>
2021-10-27 14:49:42 +0700

    chore: init commit

---
+++ Runtime/SimpleMath.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToolsPox
+{
+    public class SimpleMath 
+    {
+        public float Addition(float x, float y)
+        {
+            return x + y;
+        }
+
+        public float Subtraction(float x, float y)
+        {
+            return x - y;
+        }
+
+        public float Multiplication(float x, float y)
+        {
+            return x * y;
+        }
+
+        public float Division(float x, float y)
+        {
+            return x / y;
+        }
+    }
+    
+}
+
